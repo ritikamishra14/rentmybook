dcccc9f37426ee74f42d4c58fc2f393d6719396c
RAHUL KUMAR <38152242+RAHULMERCER@users.noreply.github.com>
2020-05-05 23:15:28 +0530

    Basic api done for today for add user

--- rentmybook/social/codes/social_request.py
+++ rentmybook/social/codes/social_request.py
@@ -1,39 +1,40 @@
 from social.codes.social_config import social_configuration
 from django.http import JsonResponse
-import json, traceback
-
-
+import json
+import traceback
+from django.views.decorators.csrf import csrf_exempt
 social_instance = social_configuration()
 
-
+@csrf_exempt
 def add_user(request):
 
-    try:
-        email_id = str(request.POST.get('email_id'))
-        gender = str(request.POST.get('gender'))
-        date_of_creation = str(request.POST.get('date_of_creation'))
-        user_image = str(request.POST.get('user_image'))
-        name = str(request.POST.get('name'))
-        address1 = str(request.POST.get('address1'))
-        address2 = str(request.POST.get('address2'))
-        landmark = str(request.POST.get('landmark'))
-        state = str(request.POST.get('state'))
-        city = str(request.POST.get('city'))
-        zip_code = str(request.POST.get('zip_code'))
-        status = str(request.POST.get('status'))
-        contact = str(request.POST.get('contact'))
-        time_zone = str(request.POST.get('time_zone'))
-        user_lat = str(request.POST.get('user_lat'))
-        user_lng = str(request.POST.get('user_lng'))
+    payload = json.loads(request.body)
+    print(payload)
 
-        if social_instance.add_user(email_id, gender ,date_of_creation,
+    try:
+        email_id = str(payload['email_id'])
+        gender = str(payload['gender'])
+        user_image = str(payload['user_image'])
+        name = str(payload['name'])
+        address1 = str(payload['address1'])
+        address2 = str(payload['address2'])
+        landmark = str(payload['landmark'])
+        state = str(payload['state'])
+        city = str(payload['city'])
+        zip_code = str(payload['zip_code'])
+        status = str(payload['status'])
+        contact = str(payload['contact'])
+        time_zone = str(payload['timezone'])
+        user_lat = str(payload['user_lat'])
+        user_lng = str(payload['user_lng'])
+
+        if social_instance.add_user(email_id, gender ,
                                     user_image, name, address1, address2,
                                     landmark, state, city, zip_code, status,
                                     contact, time_zone, user_lat, user_lng):
             jsonresponse = JsonResponse({'msg':'SUCCESS'}, status= 200 , safe=False)
             return jsonresponse
-    except Exception as _:
-
+    except Exception as e:
         traceback.print_exc()
 
     jsonresponse = JsonResponse({'msg':  'FAILURE'}, status= 500, safe=False)
